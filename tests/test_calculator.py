9ac663b1fd87a0368af32e4d29f99194948b9871
Lawwaikit <LUOWEIJIE1030@outlook.com>
2026-03-11 16:31:25 +0800

    change

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,8 +1,10 @@
 # flake8: noqa
+import allure
+
 from app.calculator import add, sub
 import pytest
 
-
+@allure.feature("计算模块")
 class TestCalculator:
     """计算器模块的测试用例"""
 
@@ -14,6 +16,7 @@ class TestCalculator:
         (10, 20, 30),
         (100, 200, 300),
     ])
+    @allure.story("加法模块")
     def test_add(self, a, b, expected):
         """测试加法功能"""
         assert add(a, b) == expected
@@ -25,6 +28,7 @@ class TestCalculator:
         (-1, -1, 0),
         (100, 50, 50),
     ])
+    @allure.story("减法模块")
     def test_sub(self, a, b, expected):
         """测试减法功能"""
         assert sub(a, b) == expected
